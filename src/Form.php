c24e3bb87d0c672fa3642508c85e91c1f74af2ba
Syone <syone7@gmail.com>
2022-11-30 00:29:08 +0100

    Form execute init on added event now

--- src/Form.php
+++ src/Form.php
@@ -3,37 +3,76 @@ namespace Sy\Component\Html;
 
 abstract class Form extends Form\FieldContainer {
 
-	private static $isSubmit = false;
-
+	/**
+	 * @var string
+	 */
 	private $formId;
 
+	/**
+	 * @var bool
+	 */
+	private $initialized;
+
+	/**
+	 * @var bool
+	 */
+	private static $isSubmit = false;
+
+	/**
+	 * Form inputs must be added in this method
+	 */
+	abstract public function init();
+
+	/**
+	 * Form submit action must be implemented in this method
+	 */
+	abstract public function submitAction();
+
+	/**
+	 * @param array $attributes The form attributes
+	 */
 	public function __construct(array $attributes = array()) {
 		parent::__construct();
+		$this->initialized = false;
 		$this->setTemplateFile(__DIR__ . '/Form/templates/Form.tpl', 'php');
 		$this->formId = $this->getFormActionTrigger();
 		$this->setAttributes($attributes);
 		$this->setOption('error-class', 'error');
 		$this->setOption('success-class', 'success');
-		$this->init();
-		if (($this->request('sy-form-action-trigger') === $this->formId) and !self::$isSubmit) {
-			self::$isSubmit = true;
-			$info = $this->getDebugTrace();
-			$info['type'] = 'Form submit';
-			$message = 'Call method ' . get_class($this) . '::submitAction';
-			$this->log($message, $info);
-			$this->submitAction();
-		}
+		$this->added(function () {
+			$this->initialize();
+			if (($this->request('sy-form-action-trigger') === $this->formId) and !self::$isSubmit) {
+				self::$isSubmit = true;
+				$info = $this->getDebugTrace();
+				$info['type'] = 'Form submit';
+				$message = 'Call method ' . get_class($this) . '::submitAction';
+				$this->log($message, $info);
+				$this->submitAction();
+			}
+		});
+		$this->mount(function () {
+			$this->setVar('ACTION_TRIGGER', $this->formId);
+			if (is_null($this->getAttribute('action'))) {
+				$this->setAttribute('action', isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '');
+			}
+			if (is_null($this->getAttribute('method'))) {
+				$this->setAttribute('method', 'post');
+			}
+		});
 	}
 
-	public function __toString() {
-		$this->setVar('ACTION_TRIGGER', $this->formId);
-		if (is_null($this->getAttribute('action'))) {
-			$this->setAttribute('action', isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '');
-		}
-		if (is_null($this->getAttribute('method'))) {
-			$this->setAttribute('method', 'post');
-		}
-		return parent::__toString();
+	/**
+	 * Initialize the form
+	 *
+	 * @param callable|null $preInit Closure executed before init
+	 * @param callable|null $postInit Closure executed after init
+	 */
+	public function initialize($preInit = null, $postInit = null) {
+		if ($this->initialized) return;
+		if (is_callable($preInit)) $preInit();
+		$this->init();
+		if (is_callable($postInit)) $postInit();
+		$this->initialized = true;
 	}
 
 	/**
@@ -48,25 +87,22 @@ abstract class Form extends Form\FieldContainer {
 	/**
 	 * Process form validation
 	 *
-	 * @param array $values
+	 * @param  array $values
 	 * @throws Form\Exception
 	 */
 	public function validate(array $values) {
 		if (!$this->isValid($values)) {
-			throw new Form\Exception;
+			throw new Form\Exception();
 		}
 	}
 
-	abstract public function init();
-
-	abstract public function submitAction();
-
+	/**
+	 * Return an id used for catching form submit action
+	 *
+	 * @return string
+	 */
 	protected function getFormActionTrigger() {
 		return md5(get_class($this));
 	}
 
 }
-
-namespace Sy\Component\Html\Form;
-
-class Exception extends \Exception {}
